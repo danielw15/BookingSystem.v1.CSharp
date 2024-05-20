a684225aecbf253c354ca8939c94666e263a69c1
17dawa01 <danielwallen1233@gmail.com>
2024-05-20 22:00:59 +0200

    More features and design fixes for a more clear overview of the Booking-object and essential information. Also design changes to the JS-code.

--- BookingSystem/Controllers/BookingsController.cs
+++ BookingSystem/Controllers/BookingsController.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using BookingSystem.Data;
 using BookingSystem.Models;
 using Microsoft.IdentityModel.Tokens;
+using System.ComponentModel;
 
 namespace BookingSystem.Controllers
 {
@@ -29,18 +30,20 @@ namespace BookingSystem.Controllers
             {
                 return Problem("Entity set 'BookingSystemContext.Booking' is null.");
             }
-           
-            var bookings = _context.Booking.Include(b => b.User).Include(b => b.Event)
-            .Where(b => (searchString == "" || (b.User.FirstName.Contains(searchString) 
-            || b.User.LastName.Contains(searchString)))
-            && (bookingEventId == 0 || b.Event.Id == bookingEventId));
+
+            var bookings = _context.Booking
+                .Include(b => b.User)
+                .Include(b => b.Event)
+                .Where(b => (searchString == "" 
+                || (b.User.FirstName.Contains(searchString) || b.User.LastName.Contains(searchString)))
+                && (bookingEventId == 0 || b.Event.Id == bookingEventId));
 
             IQueryable<Booking> bookingQuery = _context.Booking;
 
             var bookingUserVM = new BookingUserViewModel
             {
-                BookingEventsIds = new SelectList(await bookingQuery.Select(b => b.Event.Id).ToListAsync()),
-                BookingUsers = new SelectList(await bookingQuery.Select(b=> b.User).ToListAsync()),
+                BookingEventsIds = new SelectList(await bookingQuery.Select(b => b.Event!.Id).ToListAsync()),
+                BookingUsers = new SelectList(await bookingQuery.Select(b => b.User).ToListAsync()),
                 BookingEvents = new SelectList(await bookingQuery.Select(b => b.Event).ToListAsync()),
                 Bookings = await bookings.ToListAsync()
             };
@@ -53,7 +56,7 @@ namespace BookingSystem.Controllers
         {
             return "From [HttpPost]Index: filter on " + searchString;
         }
-    
+
 
         // GET: Bookings/Details/5
         public async Task<IActionResult> Details(int? id)
@@ -178,12 +181,66 @@ namespace BookingSystem.Controllers
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
+        // GET: Events/CheckBookingEventIdExists/9
+        public async Task<IActionResult> CheckBookingEventIdExists(int id)
+        {
+            var bookingEventExists = await BookingEventExists(id);
+            var eventObject = _context.Event.Find(id);
+            var dateObject = DateTime.Now;
+            string? eventTitle = null;
+            string? eventDescription = null;
+            string? eventDate = null;
+            string? eventStartTime = null;
+            string? eventEndTime = null;
+            if (bookingEventExists && eventObject != null)
+            {
+                dateObject = eventObject.Date;
+                eventTitle = eventObject.Title;
+                eventDescription = eventObject.Description;
+                eventDate = eventObject.Date.ToString("yyyy/MM/dd");
+                eventStartTime = eventObject.StartTime.ToString("HH:mm");
+                eventEndTime = eventObject.EndTime.ToString("HH:mm");
+            }
+            return Ok(new
+            {
+                exists = bookingEventExists,
+                date = dateObject,
+                EventTitle = eventTitle,
+                EventDescription = eventDescription,
+                EventDate = eventDate,
+                EventStartTime = eventStartTime,
+                EventEndTime = eventEndTime
+            });
+        }
 
+        // GET: Events/CheckBookingUserIdExists/8
+        public async Task<IActionResult> CheckBookingUserIdExists(int id)
+        {
+            var bookingUserExists = await BookingUserExists(id);
+            var user = _context.User.Find(id);
+            string? userFirstName = null;
+            string? userLastName = null;
+            if (bookingUserExists && user != null)
+            {
+                userFirstName = user.FirstName;
+                userLastName = user.LastName;
+            }
+            return Ok(new { exists = bookingUserExists, FullName = userFirstName + " " + userLastName });
+        }
+        private async Task<bool> BookingUserExists(int id)
+        {
+            return await _context.User.AnyAsync(e => e.Id == id);
+        }
+        private async Task<bool> BookingEventExists(int id)
+        {
+            return await _context.Event.AnyAsync(e => e.Id == id);
+        }
+        
         private bool BookingExists(int id)
         {
             return _context.Booking.Any(e => e.Id == id);
         }
 
-       
+
     }
 }
